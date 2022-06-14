5d2a980100fd960f00da8af7e1a6d2cb07a6a75f
bartek <bartektrybalaa@gmail.com>
2022-06-14 21:04:41 +0200

    test: test encoder and decoder mechanism

--- weirdtext/encoder/tests.py
+++ weirdtext/encoder/tests.py
@@ -1,10 +1,11 @@
-import json
 import copy
+import json
 from django.test import TestCase
 from rest_framework.test import APIRequestFactory
 
 from encoder.views import EncodeApi, DecodeApi
-from encoder.encoder import weirdtext_encoder, weirdtext_decoder
+from encoder.encoder import weirdtext_encoder, weirdtext_decoder,\
+    SEPARATOR, extract_encoded_text
 
 
 TEST_ORIGINAL_TEXT = "This is a short (test) sentence,\nbut different than in task.\
@@ -134,3 +135,93 @@ class ApiDecodeTest(TestCase):
         content_type="application/json")
         response = self.view(request)
         assert response.status_code == 400
+
+
+class TestEncoderMechanism(TestCase):
+    """
+    Test if encoder algorithm is implement with correct functionals.
+    """
+    def test_correct_words(self):
+        """
+        Test if encoder shuffle suitable words.
+        """
+        text_where_all_words_should_shuffled = "Text where words should shuffled"
+        encoded_text, word_list = weirdtext_encoder(text_where_all_words_should_shuffled)
+        original_words = text_where_all_words_should_shuffled.split()
+        # check if all words from text are in result
+        assert sorted(original_words, key=lambda s: s.lower()) == word_list
+        # check if every word is shuffled
+        assert not any(enc == org for enc, org in zip(encoded_text.split(), original_words))
+
+    def test_three_letter_word(self):
+        """
+        Test if encoder doesn't shuffle three letter word.
+        """
+        three_letter_word = "cat"
+        encoded_text, word_list = weirdtext_encoder(three_letter_word)
+        assert encoded_text == SEPARATOR + three_letter_word + SEPARATOR
+        assert word_list == []
+
+    def test_one_letter_middle_repeated(self):
+        """
+        Test if encoder doesn't shuffle word with repeated
+        the same letter in the middle.
+        """
+        repeated_one_letter_middle = "hoooot"
+        encoded_text, word_list = weirdtext_encoder(repeated_one_letter_middle)
+        assert encoded_text == SEPARATOR + repeated_one_letter_middle + SEPARATOR
+        assert word_list == []
+
+    def test_punctation_marks_string(self):
+        """
+        Test string with punctation marks.
+        Check if word was shuffled correctly and punctation marks remained unchanged.
+        """
+        correct_word = "word"
+        only_punctation_string = f"./,;']{correct_word}...,,,=-)()!\n\n!!"
+        encoded_text, word_list = weirdtext_encoder(only_punctation_string)
+        encoded_text = extract_encoded_text(encoded_text)
+
+        # get shuffled word
+        encoded_correct_word = extract_encoded_text(weirdtext_encoder(correct_word)[0])
+        assert encoded_text == f"./,;']{encoded_correct_word}...,,,=-)()!\n\n!!"
+        assert word_list == [correct_word]
+
+
+class TestDecoderMechanism(TestCase):
+    """
+    Test if decoder algorithm is implement with correct functionals.
+    """
+    def test_correct_cycle_encode_decode(self):
+        """
+        Test if decoder correctly decodes encoded text.
+        """
+        encoded_text, word_list = weirdtext_encoder(TEST_ORIGINAL_TEXT)
+        decoded_text = weirdtext_decoder(encoded_text, word_list, TEST_ORIGINAL_TEXT)
+        assert decoded_text == TEST_ORIGINAL_TEXT
+
+    def test_decode_with_punctation_marks(self):
+        """
+        Test if decoder correctly decodes text that contains punctation marks.
+        """
+        text_with_puncation = "This is (text), with many. puntation marks (e.i. big!)."
+        encoded_text, word_list = weirdtext_encoder(text_with_puncation)
+        decoded_text = weirdtext_decoder(encoded_text, word_list, text_with_puncation)
+        assert decoded_text == text_with_puncation
+
+    def test_raises_error_for_incorrect_input(self):
+        """
+        Test if decoder raises error when given encoded_text with word_list
+        doesn't map on decoder output from original text.
+        """
+        # incorrect `word_list`
+        encoded_text, word_list = weirdtext_encoder(TEST_ORIGINAL_TEXT)
+        word_list.append("additional_word")
+        with self.assertRaisesMessage(ValueError, "Incorrect encoded text."):
+            weirdtext_decoder(encoded_text, word_list, TEST_ORIGINAL_TEXT)
+
+        # incorrect `encoded_text`
+        encoded_text, word_list = weirdtext_encoder(TEST_ORIGINAL_TEXT)
+        encoded_text += "add"
+        with self.assertRaisesMessage(ValueError, "Incorrect encoded text."):
+            weirdtext_decoder(encoded_text, word_list, TEST_ORIGINAL_TEXT)
