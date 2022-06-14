1af5eda97e955f1eca041944b2060379c90f14a8
bartek <bartektrybalaa@gmail.com>
2022-06-14 18:21:07 +0200

    fix: swagger page without auth

--- weirdtext/entrypoint.sh
+++ weirdtext/entrypoint.sh
@@ -1,6 +1,8 @@
 
 #!/bin/bash
 
+python manage.py collectstatic --no-input
+
 python manage.py migrate --no-input
 
 gunicorn weirdtext.wsgi:application --bind 0.0.0.0:8000
--- weirdtext/weirdtext/urls.py
+++ weirdtext/weirdtext/urls.py
@@ -13,10 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 
 
 urlpatterns = [
     path('v1/', include('encoder.urls')),
     path('doc/', include('swagger.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
