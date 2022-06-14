1a5d356b7fe6b6a40d9449bbc7c3b76f8801b93a
bartek <bartosz.trybala@korbank.pl>
2022-06-14 09:18:39 +0200

    feat: better encoder

--- encoder.py
+++ encoder.py
@@ -5,22 +5,8 @@ import re
 
 
 ORIGINAL_TEXT = """This is a long looong test sentence,\nwith some big (biiiiig) words!"""
-PUNCTATION_PATTERN = r"\w+|[^\w\s]|\n"
 no_punctation_token = re.compile(r'(\w+)', re.U)
 
-def join_punctuation(seq, characters='.,;?!'):
-    """Auxliary function for join all digits correctly."""
-    characters = set(characters)
-    seq = iter(seq)
-    current = next(seq)
-
-    for nxt in seq:
-        if nxt in characters:
-            current += nxt
-        else:
-            yield current
-            current = nxt
-    yield current
 
 def encode(original_text):
     """
@@ -31,14 +17,13 @@ def encode(original_text):
 
     Returns shuffled text and sorted list of original words.
     """
-    words = re.findall(PUNCTATION_PATTERN, original_text, re.U)
+    words = re.split(no_punctation_token, original_text)
     shuffled_original_worlds = []
     for i, word in enumerate(words):
         # 4 digits is minimum to make shuffle possibly
         # skip punctations marks
         if len(word) < 4 or re.match(no_punctation_token, word) is None:
             continue
-
         middle_of_the_word = word[1:-1]
         # check if middle of the word doesn't contains repeated one letter
         if middle_of_the_word == len(middle_of_the_word) * middle_of_the_word[0]:
@@ -49,9 +34,9 @@ def encode(original_text):
             random_middle = ''.join(random.sample(middle_of_the_word, len(middle_of_the_word)))
             word = word[0] + random_middle + word[-1]
         words[i] = word
-        shuffled_original_worlds.append(word)
+        shuffled_original_worlds.append(original_word)
 
-    encoded_text = " ".join(join_punctuation(words))
+    encoded_text = "".join(words)
     return encoded_text, sorted(shuffled_original_worlds, key=lambda s: s.lower())
 
 
